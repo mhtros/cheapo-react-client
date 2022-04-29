b7f14b8ae54893d6d10e559d76a2cda6dee1b1d8
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-29 12:43:23 +0300

    Implement UnauthorizePageTemplate component

---
+++ src/components/layout/unauthorize-page-template/UnauthorizePageTemplate.js
@@ -0,0 +1,21 @@
+import { Space, Typography } from "antd";
+import CenteredContainer from "../../../components/layout/centered-container/CenteredContainer";
+
+const { Text, Title } = Typography;
+
+const UnauthorizePageTemplate = ({ children }) => (
+  <CenteredContainer>
+    <div>
+      <Space
+        direction="vertical"
+        style={{ textAlign: "center", marginBottom: "2rem" }}
+      >
+        <Title style={{ margin: 0 }}>CHEAPO</Title>
+        <Text>A simple way to keep track your monthly expenses.</Text>
+      </Space>
+      {children}
+    </div>
+  </CenteredContainer>
+);
+
+export default UnauthorizePageTemplate;
