ee801f0ec41c3f1df4ff5683efea1bcbecdc2596
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-29 17:43:22 +0300

    Remove dark theme logic from App.js refactor code

--- src/App.js
+++ src/App.js
@@ -2,34 +2,25 @@ import { useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
 import UnauthorizePageTemplate from "./components/layout/unauthorize-page-template/UnauthorizePageTemplate";
 import authenticationContext from "./context/authentication-context";
-import themeContext from "./context/theme-context";
 import AuthorizeRoutes from "./pages/routes/AuthorizeRoutes";
 import UnauthorizeRoutes from "./pages/routes/UnauthorizeRoutes";
 
 function App() {
   const logged = useContext(authenticationContext).logged;
-  const isDark = useContext(themeContext).dark;
 
-  const lightTheme = () => {
-    document.body.classList.add("body");
-    document.body.classList.remove("body--dark");
-  };
-
-  const darkTheme = () => {
-    document.body.classList.add("body--dark");
-    document.body.classList.remove("body");
-  };
-
-  isDark ? darkTheme() : lightTheme();
-
-  const routes = logged ? (
-    <AuthorizeRoutes />
-  ) : (
+  const UnauthorizeTemplate = (
     <UnauthorizePageTemplate>
       <UnauthorizeRoutes />
     </UnauthorizePageTemplate>
   );
-  return <BrowserRouter>{routes}</BrowserRouter>;
+
+  const AuthorizeTemplate = <AuthorizeRoutes />;
+
+  return (
+    <BrowserRouter>
+      {logged ? AuthorizeTemplate : UnauthorizeTemplate}
+    </BrowserRouter>
+  );
 }
 
 export default App;
