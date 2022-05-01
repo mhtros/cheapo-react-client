ca790d77ebeaba931d7929be52ef88c9d0fec2d1
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-05-02 00:10:31 +0300

    Update AuthorizeRoutes

--- src/pages/routes/AuthorizeRoutes.js
+++ src/pages/routes/AuthorizeRoutes.js
@@ -1,10 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "../dashboard/Dashboard";
-import NotFound from "../not-found/NotFound";
+import Dashboard from "../Dashboard";
+import NotFound from "../NotFound";
+import Password from "../settings/Password";
+import PersonalData from "../settings/PersonalData";
+import Profile from "../settings/Profile";
+import TwoFactorAuthentication from "../settings/TwoFactorAuthentication";
 
 const AuthorizeRoutes = () => (
   <Routes>
     <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/settings/profile" element={<Profile />} />
+    <Route path="/settings/password" element={<Password />} />
+    <Route
+      path="/settings/two-factor-authentication"
+      element={<TwoFactorAuthentication />}
+    />
+    <Route path="/settings/personal-data" element={<PersonalData />} />
     {/* Fall back routes */}
     <Route path="/" element={<Dashboard />} />
     <Route path="*" element={<NotFound />} />
