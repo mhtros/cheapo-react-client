5595f2dfcbf0c347300a7fefe3eaed4f64187821
mhtros <mitropanos95@gmail.com>
2022-05-28 17:41:02 +0300

    Add revoke token

--- src/pages/settings/Profile.js
+++ src/pages/settings/Profile.js
@@ -1,5 +1,5 @@
 import { InboxOutlined } from "@ant-design/icons";
-import { Button, Card, Form, Upload } from "antd";
+import { Button, Card, Form, Typography, Upload } from "antd";
 import ImgCrop from "antd-img-crop";
 import React, { useContext, useState } from "react";
 import { apiUri } from "../../appsettings";
@@ -8,12 +8,15 @@ import { errorToast, successToast } from "../../helpers/toasts";
 import { useHttp } from "../../hooks/http-hook";
 
 const { Dragger } = Upload;
+const { Paragraph } = Typography;
 
 const Profile = () => {
   const { httpCall } = useHttp();
   const authenticationCtx = useContext(authenticationContext);
 
   const [loading, setLoading] = useState(false);
+  const [extraLoading, setExtraLoading] = useState(false);
+
   const [fileList, setFileList] = useState([]);
 
   const updateImageHandler = async () => {
@@ -54,39 +57,84 @@ const Profile = () => {
     return false;
   };
 
-  const title = <div style={{ textAlign: "center" }}>Update Profile Image</div>;
+  const revokeTokenHandler = async (event) => {
+    event.preventDefault();
+    setExtraLoading(true);
+    try {
+      const url = `${apiUri}/authentication/revoke`;
+      await httpCall(url, { method: "POST" });
+      successToast("Token revoked successfully");
+      authenticationCtx.signout();
+    } catch (ex) {
+      setLoading(false);
+    }
+  };
+
+  const profileImageTitle = (
+    <div style={{ textAlign: "center" }}>Update Profile Image</div>
+  );
+
+  const revokeTokenTitle = (
+    <div style={{ textAlign: "center" }}>Revoke Token</div>
+  );
 
   return (
-    <Card title={title}>
-      <Form onFinish={updateImageHandler}>
-        <Form.Item>
-          <ImgCrop rotate>
-            <Dragger
-              fileList={fileList}
-              style={{ marginBottom: "0.5rem" }}
-              beforeUpload={changeImageHandler}
-              onRemove={() => setFileList([])}
-              accept="image/jpeg"
-              listType="picture"
-            >
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              <p className="ant-upload-text">Upload an image</p>
-              <p className="ant-upload-hint">
-                Click or drag file to this area to upload
-              </p>
-            </Dragger>
-          </ImgCrop>
-        </Form.Item>
+    <>
+      <Card title={profileImageTitle}>
+        <Form onFinish={updateImageHandler}>
+          <Form.Item>
+            <ImgCrop rotate>
+              <Dragger
+                fileList={fileList}
+                style={{ marginBottom: "0.5rem" }}
+                beforeUpload={changeImageHandler}
+                onRemove={() => setFileList([])}
+                accept="image/jpeg"
+                listType="picture"
+              >
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">Upload an image</p>
+                <p className="ant-upload-hint">
+                  Click or drag file to this area to upload
+                </p>
+              </Dragger>
+            </ImgCrop>
+          </Form.Item>
 
-        <div style={{ display: "flex", flexDirection: "row-reverse" }}>
-          <Button loading={loading} type="primary" htmlType="submit">
-            Update
-          </Button>
-        </div>
-      </Form>
-    </Card>
+          <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+            <Button
+              loading={loading}
+              type="primary"
+              htmlType="submit"
+              disabled={extraLoading}
+            >
+              Update
+            </Button>
+          </div>
+        </Form>
+      </Card>
+
+      <Card title={revokeTokenTitle} style={{ marginTop: "1rem" }}>
+        <Paragraph>
+          If you proceed with token revocation you will automatically sign out
+          of the application and any further request made with it will be
+          rejected.
+        </Paragraph>
+        <Button
+          style={{ width: "100%" }}
+          size="large"
+          danger
+          onClick={revokeTokenHandler}
+          loading={extraLoading}
+          disabled={loading}
+          type="primary"
+        >
+          Revoke token
+        </Button>
+      </Card>
+    </>
   );
 };
 
