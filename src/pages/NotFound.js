86991dc15fb9c609ef5525e3f25a5adc1b957a43
mhtros <mitropanos95@gmail.com>
2022-06-15 23:15:19 +0300

    Completion of NotFound component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,38 @@
-const NotFound = () => <h1>Not Found</h1>;
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button, Card, Result } from "antd";
+import { Link } from "react-router-dom";
+import Robot404 from "../components/svgs/Robot404";
+
+const linkStyle = {
+  fontSize: "10px",
+  color: "var(--base-color-300)",
+  display: "flex",
+  flexDirection: "row-reverse",
+};
+
+const NotFound = () => (
+  <Card>
+    <Result
+      icon={<Robot404 />}
+      title="Uh oh..."
+      subTitle="We couldn't find the page you were looking for!"
+      extra={
+        <Button size="large" type="primary">
+          <Link to="dashboard">
+            <ArrowLeftOutlined /> Go to dashboard
+          </Link>
+        </Button>
+      }
+    />
+    <a
+      style={linkStyle}
+      href="https://storyset.com/web"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Web illustrations by Storyset
+    </a>
+  </Card>
+);
 
 export default NotFound;
