6fa7ea1ba9005f2b75d323da033f23edb7303dfd
mhtros <mitropanos95@gmail.com>
2022-06-27 21:15:53 +0300

    Change logos clear unused files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CombinedProvider from "./context/CombinedProvider";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,8 +11,3 @@ root.render(
     <App />
   </CombinedProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
